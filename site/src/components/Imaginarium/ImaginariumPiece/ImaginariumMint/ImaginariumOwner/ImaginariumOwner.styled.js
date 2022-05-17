cf29367387d0684bb0c81f8348ef56be601864e9
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2022-05-17 02:15:43 -0400

    style token owner

---
+++ site/src/components/Imaginarium/ImaginariumPiece/ImaginariumMint/ImaginariumOwner/ImaginariumOwner.styled.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ImaginariumOwner = styled.div`
+	font-family: wix1;
+	font-size: 11px;
+	margin-top: 16px;
+`;
---
+++ site/src/components/Imaginarium/ImaginariumPiece/ImaginariumMint/hooks/useToggleTokenVideoState.js
@@ -0,0 +1,73 @@
+import { useContractFunction, useEthers } from "@usedapp/core";
+import { useChainConfig } from "common/hooks/useChainConfig";
+import { utils } from "ethers";
+import { useEffect, useState } from "react";
+
+export const useToggleTokenVideoState = ({ tokenId }) => {
+	const { contract } = useChainConfig();
+	const { account } = useEthers();
+	const [error, setError] = useState(undefined);
+	const [isToggling, setIsToggling] = useState(false);
+
+	const { send, state } = useContractFunction(
+		contract,
+		"toggleTokenVideoState",
+		{
+			transactionName: "ToggleTokenVideoState",
+		}
+	);
+
+	// handle successful mints
+	useEffect(() => {
+		if (
+			state &&
+			state.status !== "None" &&
+			state.status !== "Success" &&
+			state.status !== "Exception"
+		) {
+			setIsToggling(true);
+		} else {
+			setIsToggling(false);
+		}
+	}, [state, setIsToggling]);
+
+	// handle mint error
+	useEffect(() => {
+		if (state && state.status === "Exception") {
+			setError(state.errorMessage);
+			setIsToggling(false);
+		}
+	}, [state]);
+
+	const toggleTokenVideoState = async () => {
+		setError(undefined);
+
+		if (!account) {
+			setError("No account connected");
+			return;
+		}
+
+		try {
+			await send(tokenId);
+		} catch (e) {
+			console.log(e);
+			if (e?.code === 4001) {
+				setError("User denied transaction signature");
+			} else if (e.code && e.message.includes("execution reverted")) {
+				const reason = e.message
+					.split("execution reverted: ")?.[1]
+					.split(",")[0];
+
+				setError(reason ?? "Error");
+			} else {
+				setError(e);
+			}
+		}
+	};
+
+	return {
+		toggleTokenVideoState,
+		error,
+		isToggling,
+	};
+};

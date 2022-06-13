c93cae4341bfe7fbda8341cff6370f5fe92a8e88
Andrew Gallagher <andrewtgallagherJS@gmail.com>
2022-06-13 16:19:35 -0400

    add allowlist

--- site/src/components/Imaginarium/hooks/useListPrice.js
+++ site/src/components/Imaginarium/hooks/useListPrice.js
@@ -1,8 +1,10 @@
-import { useCall } from "@usedapp/core";
+import { useCall, useEthers } from "@usedapp/core";
 import { useChainConfig } from "common/hooks/useChainConfig";
+import { listPriceAllowList } from "components/Imaginarium/hooks/listPriceAllowList";
 import { utils } from "ethers";
 
 export const useListPrice = () => {
+	const { account } = useEthers();
 	const { contract } = useChainConfig();
 	const listPriceCall = useCall({
 		contract,
@@ -12,7 +14,23 @@ export const useListPrice = () => {
 
 	const listPrice = listPriceCall?.value?.[0] ?? undefined;
 
+	if (
+		listPrice &&
+		account &&
+		listPriceAllowList.includes(account.toLowerCase())
+	) {
+		return {
+			listPrice: utils.formatEther(listPrice),
+		};
+	}
+
+	if (listPrice) {
+		return {
+			listPrice: "2.2",
+		};
+	}
+
 	return {
-		listPrice: listPrice ? utils.formatEther(listPrice) : undefined,
+		listPrice: undefined,
 	};
 };
